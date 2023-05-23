5ead8dfb439c8e02a8e682f0c46b19268290bf79
Nathan Gurrin-Smith <nathan.gurrinsmith@outlook.com>
2023-05-23 17:26:03 -0400

    Updated some files

--- jobs/initDB.sh
+++ jobs/initDB.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Drop and create new database
-dropdb tctest || echo "tctest doesn't exist, moving on"
-createdb -O yewlkang -U yewlkang tctest
+# Create new database
+createdb -O postgres -U postgres tcDB
 
 # Run the init.sql script
-psql -U yewlkang -d tctest -a -f "initDB.sql"
+psql -U postgres -d tcDB -a -f "initDB.sql"
