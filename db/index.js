39c59af62654553d7c0452bb96506146164d0bea
Nathan Gurrin-Smith <nathan.gurrinsmith@outlook.com>
2023-05-23 19:10:00 -0400

    Fixing database environment variable

--- db/index.js
+++ db/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 module.exports = {
     query: (text, params) => pool.query(text, params)
